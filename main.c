4dc55ccabff31a7941da107e5ba0b405b434e82d
kewb <44578042+kewb@users.noreply.github.com>
2023-10-14 22:13:28 +0300

    Average and sum of an array

--- main.c
+++ main.c
@@ -2,18 +2,30 @@
 #include <limits.h> // Include limits.h for INT_MIN
 
 void findThreeLargest(int arr[], int size, int result[]);
+void avgSum(int arr[], int size);
 
 int main() {
-    int arr[] = {3, 4, 5, 8, 9, 1, 2, 7, 10, 0};
+    int arr[] = {5,2,7,6,5,3};
     int arraySize = sizeof(arr) / sizeof(arr[0]);
-    int result[3];
-
-    findThreeLargest(arr, arraySize, result);
-
-    printf("The three largest numbers are: %d, %d, %d\n", result[0], result[1], result[2]);
+    avgSum(arr,arraySize);
 
     return 0;
 }
+
+
+void avgSum(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum = sum + arr[i];
+    }
+    float avg = (float)sum / size;
+
+    printf("Sum of array: %d\nAvg of array: %f\n", sum, avg);
+}
+
+
+
+
 
 void findThreeLargest(int arr[], int size, int result[]) {
     for (int i = 0; i < 3; i++) {
