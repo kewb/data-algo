19abff8d3722d856a097cd853c2b1630181820a1
kewb <44578042+kewb@users.noreply.github.com>
2023-10-14 22:25:28 +0300

    Smallest even numbers in an array

--- main.c
+++ main.c
@@ -2,15 +2,41 @@
 #include <limits.h> // Include limits.h for INT_MIN
 
 void findThreeLargest(int arr[], int size, int result[]);
+void findThreeSmallestEven(int arr[], int size, int result[]);
 void avgSum(int arr[], int size);
 
 int main() {
-    int arr[] = {5,2,7,6,5,3};
+    int arr[] = {3,5,1,8,9,2,7,10,11,0};
     int arraySize = sizeof(arr) / sizeof(arr[0]);
-    avgSum(arr,arraySize);
+    int result[3];
+    findThreeSmallestEven(arr,arraySize,result);
+    printf("Smallest: %d, %d, %d\n", result[0], result[1], result[2]);
 
     return 0;
 }
+
+
+
+void findThreeSmallestEven(int arr[], int size, int result[]) {
+    for (int i = 0; i < 3; i++) {
+        result[i] = INT_MAX;
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i] % 2 != 0) {
+            for (int j = 0; j < 3; j++) {
+                if (arr[i] < result[j]) {
+                    for (int k = 2; k > j; k--) {
+                        result[k] = result[k - 1];
+                    }
+                    result[j] = arr[i];
+                    break;
+                }
+            }
+        }
+    }
+}
+
 
 
 void avgSum(int arr[], int size) {
