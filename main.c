bbdff840253d9a900b35ef6b997e5f529118516f
kewb <44578042+kewb@users.noreply.github.com>
2023-10-14 21:15:44 +0300

    Made a separate function of maxMinArray

--- main.c
+++ main.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 
+
+int maxMinArray(int arr[], int arraySize); // Function prototype
+
 int main() {
     int arr[] = {3, 5, 8, 6, 2, 7, 10, 11};
     int arraySize = sizeof(arr) / sizeof(arr[0]);
-    int min, max = arr[0];
-//    printf("%d\n", arraySize);
+    int min, max;
+
+    maxMinArray(arr, arraySize);
+
+    return 0;
+}
+
+
+int maxMinArray(int arr[], int arraySize) {
+    int min = arr[0];
+    int max = arr[0];
+
     for (int i = 0; i < arraySize; ++i) {
-        if(arr[i] < i)
+        if (arr[i] < min)
             min = arr[i];
-        if(arr[i] > max)
+        if (arr[i] > max)
             max = arr[i];
     }
+
     printf("Smallest elem: %d\n and biggest elem: %d\n", min, max);
-    return 0;
+
+    return min;
 }
